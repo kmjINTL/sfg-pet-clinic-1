3d27fd701e10033cc7ff738d5b40c28b4d7974d6
Keanu-Johston <johnstonkeanu3@gmail.com>
2020-12-22 15:42:18 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic1/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic1.model;
+
+public class Person {
+    private String Firstname;
+    private String lastName;
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic1/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgpetclinic1.model;
+
+public class Vet extends Person{
+
+}
