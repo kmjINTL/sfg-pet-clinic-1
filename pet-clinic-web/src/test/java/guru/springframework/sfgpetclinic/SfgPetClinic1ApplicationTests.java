a835e07fcfa5dfd1157c95f0e4a024c306ac0d31
kmj <johnstonkeanu3@gmail.com>
2020-12-28 15:59:11 +0200

    Edited petclinic-data

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinic1ApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinic1ApplicationTests.java
@@ -1,14 +1,13 @@
-package sfgpetclinic1;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinic1ApplicationTests {
-
     @Test
     public void contextLoads() {
     }
