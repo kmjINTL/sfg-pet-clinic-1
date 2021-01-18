f621aed048fb455d15f86533356c9eb2b2998949
kmjINTL <johnstonkeanu3@gmail.com>
2021-01-18 14:59:47 +0200

    adding Spring Config. CLoses #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 @Service
 @Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
